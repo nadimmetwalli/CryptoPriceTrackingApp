06da027be9d2f6f1e95daf50e7e4c150cb09ab04
Nadim Metwalli <nadimmetwalli@gmail.com>
2025-06-13 01:32:54 +0300

    Creating the coin page

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Coin.css'
+import {useParams} from 'react-router-dom'
 
 const Coin = () => {
+  const {coinId} = useParams();
   return (
     <div>
-      <h2>Coin</h2>
+      <h2>Coin : {coinId}</h2>
     </div>
   )
 }
